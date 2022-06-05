2cc4cc9d022851225aa79ddc09def2b7ab63e938
DanielRGilles <danryangilles@gmail.com>
2022-06-05 15:07:38 -0800

    set up react router

--- landing-page/server/server.js
+++ landing-page/server/server.js
@@ -6,7 +6,7 @@ app.use(express.static(path.join(__dirname, '..', 'build')));
 app.use(express.static('public'));
 
 app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "..", "public", "index.html"));
   });
   
 app.listen(3000, () => console.log('App is a listennin on port boy 3000.'));
