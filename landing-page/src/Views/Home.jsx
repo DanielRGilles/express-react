f17bdbeeb80d4d98643784b5a403c756bdc387fb
DanielRGilles <danryangilles@gmail.com>
2022-07-28 12:46:36 -0800

    added component layout

---
+++ landing-page/src/Views/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../Components/Footer/Footer'
+import Header from '../Components/Header/Header'
+import NavBar from '../Components/Nav/NavBar'
+
+export default function Home() {
+  return (
+    <>
+        <Header/>
+        <NavBar/>
+        <Footer/>
+    </>
+  )
+}
